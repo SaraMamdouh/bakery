04d300106264492467f1517efc3e9dcd17f7823b
sara <smamdouh.1995@gmail.com>
2020-08-28 22:48:51 +0200

    wrong

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Index from './index'
+import Index from './component/index'
 import App from './App';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
