70a590c13289530ccaa74a8b68de50ef54fa471a
sara <smamdouh.1995@gmail.com>
2020-08-28 20:56:18 +0200

    Updates

---
+++ precache-manifest.0fc751527f79485518ff4e9bad272c52.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fda644851ce5943d0cb707d0117ba1",
+    "url": "/bakery/index.html"
+  },
+  {
+    "revision": "a180cd4c0d776abfd481",
+    "url": "/bakery/static/css/2.9a67dc5d.chunk.css"
+  },
+  {
+    "revision": "92e3d8cb0fdb44d06f1a",
+    "url": "/bakery/static/css/main.183e6124.chunk.css"
+  },
+  {
+    "revision": "a180cd4c0d776abfd481",
+    "url": "/bakery/static/js/2.f10a981a.chunk.js"
+  },
+  {
+    "revision": "3d5a4c66a8bccdff8b500354f5775d85",
+    "url": "/bakery/static/js/2.f10a981a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e3d8cb0fdb44d06f1a",
+    "url": "/bakery/static/js/main.5c97fdbf.chunk.js"
+  },
+  {
+    "revision": "67d39f1948295104a1b6",
+    "url": "/bakery/static/js/runtime-main.47277509.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/bakery/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/bakery/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/bakery/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/bakery/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/bakery/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/bakery/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/bakery/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/bakery/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/bakery/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/bakery/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
